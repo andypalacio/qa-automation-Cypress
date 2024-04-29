6c3505e8a923e1040f9c3f84b243419cf0d47966
Andy Palacio <45830411+andypalacio@users.noreply.github.com>
2024-04-28 23:30:16 -0300

    setup for allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,36 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
 const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+const {allureCypress, AllureCypress} = require("allure-cypress/reporter");
 
 async function setupNodeEvents(on, config) {
-  await addCucumberPreprocessorPlugin(on, config);
+    await addCucumberPreprocessorPlugin(on, config);
 
-  on(
-      "file:preprocessor",
-      createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      })
-  );
-
-  // Make sure to return the config object as it might have been modified by the plugin.
-  return config;
+    on(
+        "file:preprocessor",
+        createBundler({
+            plugins: [createEsbuildPlugin(config)],
+        })
+    );
+    allureCypress(on, {
+        resultsDir: "./allure-results"
+    });
+    // Make sure to return the config object as it might have been modified by the plugin.
+    return config;
 }
 
 module.exports = defineConfig({
-  e2e: {
-    specPattern: "**/*.feature",
-    excludeSpecPattern: [
-      "*.js",
-      "*.md"
-    ],
-    chromeWebSecurity: false,
-    projectId: "bfi83g",
-    supportFile: false,
-    numTestsKeptInMemory: 0,
-    setupNodeEvents
-  }
-
+    e2e: {
+        specPattern: "**/*.feature",
+        excludeSpecPattern: [
+            "*.js",
+            "*.md"
+        ],
+        chromeWebSecurity: false,
+        projectId: "bfi83g",
+        supportFile: false,
+        numTestsKeptInMemory: 0,
+        setupNodeEvents,
+    }
 })
